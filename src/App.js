d4f8c9fa32f65d80d29c89f3912cc59399f99fa0
ivystrayed <ivystrayed@gmail.com>
2023-02-10 18:31:42 -0800

    got map displaying bench markers, code in its own componant but commented out in app in case i choose to not keep it in its own componant

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 
 import './App.css';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import data from './dummidata.json'
+// import { MapContainer, TileLayer, Marker, Popup,} from 'react-leaflet'
+// import markerIconPng from "leaflet/dist/images/marker-icon.png"
+// import {Icon} from 'leaflet'
+import Map from "./components/Map"
 
 function App() {
-  return (
-    <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id='map'>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  
+  // need a function to create the average rating to send with POST requests
+  
+
+  
+  // const allMarkers = data.map((marker) => {
+      
+  //   return (
+  //         <Marker key={marker.id} position={[marker.long, marker.lat]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+  //             <Popup>
+  //                 its a bench <br /> yeah
+  //             </Popup>
+  //         </Marker>
+  //     );  
+  // })
+  
+  // return (
+  //     <MapContainer center={[47.6062, -122.3321]} zoom={12} scrollWheelZoom={true} id='map'>
+  //       <TileLayer
+  //           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //       />
+  //       <div>
+  //         {allMarkers}
+  //       </div>
+  //     </MapContainer>)
+
+    return (
+      <Map markers={data}/>
+    )
+
+
 }
 
+
 export default App;
+
+
+// const Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// 	subdomains: 'abcd',
+// 	minZoom: 1,
+// 	maxZoom: 16,
+// 	ext: 'jpg'
+// });
---
+++ src/components/Map.js
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
+
+
+
+const Map = (props) => {
+     
+    const allMarkers = props.markers.map((marker) => {
+      
+        return (
+              <Marker key={marker.id} position={[marker.long, marker.lat]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                  <Popup>
+                      its a bench <br /> yeah
+                  </Popup>
+              </Marker>
+          );  
+      })
+      
+      return (
+          <MapContainer center={[47.6062, -122.3321]} zoom={12} scrollWheelZoom={true} id='map'>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <div>
+              {allMarkers}
+            </div>
+          </MapContainer>)
+}
+
+Map.propTypes = {
+    markers : PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        img: PropTypes.any.isRequired,
+        long: PropTypes.number.isRequired,
+        lat:  PropTypes.number.isRequired,
+      }
+))
+    }
+        
+
+
+export default Map
